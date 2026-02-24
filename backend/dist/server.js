728a2d5c8f44505949a3279641a1ceeff86ce549
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-24 19:30:59 +0530

    server setup

---
+++ backend/dist/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = 5000;
+app.get("/", (req, res) => {
+    res.send("working");
+});
+app.listen(port, () => {
+    console.log(`App is listening to port ${port}`);
+});
+//# sourceMappingURL=server.js.map
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//middleware
 const app = express();
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 const port = 5000;
 
-app.get("/",(req,res)=>{
-    res.send("working");
-})
+app.use(express.json());
+
 app.listen(port,()=>{
     console.log(`App is listening to port ${port}`);
 })
