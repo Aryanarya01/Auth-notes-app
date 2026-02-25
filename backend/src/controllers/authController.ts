fca57b9afae8e881f81b29ac2a3af0fbbe469219
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-25 18:03:12 +0530

    user schema interface

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -4,7 +4,10 @@ import type { IUser } from "../models/user.js";
 
 export const Register =async (req:Request,res:Response,next:NextFunction) =>{
     try{
-        let {name, email, password}: IUser
+        let {name, email, password}: IUser = req.body;
+        if(!name || !email || !password){
+            res.status(400).json({message : "All fields required!"});
+        }
     }catch(err){
         res.status(500).json({message : "Server Error!"})
     }
