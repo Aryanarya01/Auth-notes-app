3c06a7d9bc811c66502317e8ed0fa544d85d9142
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-25 17:36:01 +0530

    user schema

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,3 +1,16 @@
 import mongoose,{Document,Schema} from "mongoose";
 
-const userSchema
+const userSchema = new mongoose.Schema({
+    name : {
+        type:String,
+    },
+    email:{
+        type:String,
+    },
+    password:{
+        type:String,
+    }
+});
+
+const User = mongoose.model("User",userSchema);
+r
