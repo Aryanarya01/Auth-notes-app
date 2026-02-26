af657987513f09441d7937408ed79b72dcac27de
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-26 16:37:30 +0530

    middleware

--- backend/src/middleware/protect.ts
+++ backend/src/middleware/protect.ts
@@ -1,21 +1,24 @@
-import type {NextFunction, Request, Response } from "express";
-import  Jwt from "jsonwebtoken";
+import type { NextFunction, Request, Response } from "express";
+import Jwt from "jsonwebtoken";
 
-
-interface AuthRequest extends Request{
-    user? : any;
+interface AuthRequest extends Request {
+  user?: any;
 }
- export const protect = async(req:AuthRequest,res:Response,next:NextFunction)=>{
-    try{
-        const token = req.cookies.token;
-        if(!token){
-            res.status(401).json({message : "Not Authorized!"});
-            return;
-        }
-        const decoded = Jwt.verify(token, process.env.JWT_SECRET as string
-    ) as { id: string };
-
-    }catch(err){
-        res.status(401).json({message : "Invalid token!"})
+export const protect = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      res.status(401).json({ message: "Not Authorized!" });
+      return;
     }
- }
+    const decoded = Jwt.verify(token, process.env.JWT_SECRET as string) as {
+      id: string;
+    };
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token!" });
+  }
+};
