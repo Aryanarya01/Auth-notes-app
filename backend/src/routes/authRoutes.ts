e9a8a4c0df419f3d1e1d068ebdfce5c11656007b
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-26 16:43:44 +0530

    Protect route

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -5,6 +5,6 @@ const router = Router();
 
 router.post("/register", Register);
 router.post("/login", Login);
-router.get("/profile",protect,(req,res)=>{
-    res.json({req.user})
+router.get("/profile",protect,(req:any,res)=>{
+    res.json(req.user)
 })
