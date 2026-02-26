11a1acb90b9f750fdc82aa88b4ec263c1798c23e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-26 16:42:44 +0530

    Protect route

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,6 +1,10 @@
 import { Register, Login } from "../controllers/authController.js";
 import { Router } from "express";
+import { protect } from "../middleware/protect.js";
 const router = Router();
 
 router.post("/register", Register);
 router.post("/login", Login);
+router.get("/profile",protect,(req,res)=>{
+    res.json({req.user})
+})
