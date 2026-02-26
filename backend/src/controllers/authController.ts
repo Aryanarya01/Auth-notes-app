c0aca145e73b2b48343e06feac924e5dc863dcf4
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-26 16:16:56 +0530

    httpOnly Cookie

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -68,6 +68,13 @@ export const Login = async (
     const token = Jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
       expiresIn: "7d",
     });
+
+    res.cookie("token",token, {
+      httpOnly: true,
+      secure: false, // true in production
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    })
+
   } catch (err) {
     res.status(500).json({ message: "Server Error!" });
   }
