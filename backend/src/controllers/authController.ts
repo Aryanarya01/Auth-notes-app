b2f63a4c2482259ddc737e82009d0b7fe366af6e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-26 16:15:41 +0530

    jwt token

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -6,7 +6,6 @@ import Jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 export const Register = async (
   req: Request,
   res: Response,
@@ -30,44 +29,46 @@ export const Register = async (
       email,
       password: hashedPassword,
     });
-    res
-      .status(200)
-      .json({
-        message: "User created successfully!",
-        id: user._id,
-        name: user.name,
-        email: user.email,
-      });
+    res.status(200).json({
+      message: "User created successfully!",
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    });
   } catch (err) {
     res.status(500).json({ message: "Server Error!" });
   }
 };
 
+export const Login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    let { email, password }: { email: string; password: string } = req.body;
+    if (!email || !password) {
+      res.status(400).json({ message: "All fields required!" });
+      return;
+    }
 
-export const Login = async (req:Request,res:Response,next:NextFunction):Promise<void>=>{
-  try{
-    let {email,password}:{email:string,password:string} = req.body;
-      if(!email || !password){
-        res.status(400).json({message : "All fields required!"});
-        return;
-      }
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "User already exists!" });
+      return;
+    }
 
-      const user = await User.findOne({email});
-      if(!user){
-        res.status(400).json({message : "User already exists!"});
-        return;
-      }
+    const isMatch = await bcrypt.compare(password, user.password);
 
-      const isMatch =await bcrypt.compare(password,user.password);
+    if (!isMatch) {
+      res.status(400).json({ message: "Invalid Credentials!" });
+      return;
+    }
 
-      if(!isMatch){
-        res.status(400).json({message : "Invalid Credentials!"});
-        return;
-      }
-
-      const token = Jwt.sign({id : user._id}, process.env.JWT_SECRET as string,
-      { expiresIn: "7d" })
-  }catch(err){
-    res.status(500).json({message : "Server Error!"})
+    const token = Jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
+      expiresIn: "7d",
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server Error!" });
   }
-}
+};
