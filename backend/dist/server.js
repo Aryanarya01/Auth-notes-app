7f140a77e875e0f1ba870b49180dfd8288c215ab
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-26 17:05:42 +0530

    Protect route

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,23 +4,23 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
 import router from "./routes/authRoutes.js";
-dotenv.config();
-connectDB();
+const port = process.env.PORT || 5000;
 //middleware
 const app = express();
+dotenv.config();
+connectDB();
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
 }));
-const port = process.env.PORT || 5000;
-app.use("/api/auth", router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", router);
+app.use(cookieParser());
 app.get("/", (req, res) => {
     res.send("API running...");
 });
-Router;
 app.listen(port, () => {
     console.log(`App is listening to port ${port}`);
 });
