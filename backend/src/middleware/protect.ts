05f2b8ae26cb9cc26b0c84300c79ec8c2697a67b
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-26 16:34:54 +0530

    middleware

--- backend/src/middleware/protect.ts
+++ backend/src/middleware/protect.ts
@@ -1,4 +1,14 @@
+import type {NextFunction, Request, Response } from "express";
 import  Jwt from "jsonwebtoken";
 
 
-const protect = await(req)
+interface AuthRequest extends Request{
+    user? : any;
+}
+ export const protect = async(req:AuthRequest,res:Response,next:NextFunction)=>{
+    try{
+        
+    }catch(err){
+        res.status(401).json({message : "Invalid token!"})
+    }
+ }
