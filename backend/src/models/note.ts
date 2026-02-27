0f2e7a7c31663bec94d2db6ad39de74bf0502ff6
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-27 10:50:08 +0530

    notes model

--- backend/src/models/note.ts
+++ backend/src/models/note.ts
@@ -1,22 +1,23 @@
 import mongoose, { Model } from "mongoose";
 import type { INote } from "../types/types.js";
 
-const noteSchema = new mongoose.Schema<INote>({
-    title : {
-        type : String,
-        required : true,
+const noteSchema = new mongoose.Schema<INote>(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    content :{
-        type : String,
-        required:true,
+    content: {
+      type: String,
+      required: true,
     },
-    user :{
-          type: mongoose.Schema.Types.ObjectId,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required : true,
+      required: true,
     },
-},
-{timestamps : true});
+  },
+  { timestamps: true },
+);
 
-export const Note : Model<INote> = mongoose.model<INote>("Note",noteSchema);
-
+export const Note: Model<INote> = mongoose.model<INote>("Note", noteSchema);
