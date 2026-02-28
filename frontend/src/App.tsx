ce65876e96f9c552f673f534850fb191c7eddd55
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-28 15:42:54 +0530

    frontend setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,23 @@
- 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Profile from './pages/Profile'
-import Notes from './pages/Notes'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import Notes from "./pages/Notes";
 
 function App() {
-  
-
   return (
     <>
-  <BrowserRouter >
-    <Routes>
-      <Route path='/' element={<Login />}/>
-      <Route path='/register' element={<Register/>} />
-      <Route path='/profile' element={<Profile/>} />
-      <Route path='/notes' element={<Notes/>} />
-    </Routes>
-  </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/notes" element={<Notes />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
