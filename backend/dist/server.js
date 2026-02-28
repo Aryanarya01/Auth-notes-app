3e37d279a09ec72605f427a8ddf5c88f58b480a4
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-28 10:19:15 +0530

    get logged in user

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
 import router from "./routes/authRoutes.js";
+import noteRouter from "./routes/noteRoutes.js";
 const port = process.env.PORT || 5000;
 //middleware
 const app = express();
@@ -16,8 +17,9 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/auth", router);
 app.use(cookieParser());
+app.use("/api/auth", router);
+app.use("/api");
 app.get("/", (req, res) => {
     res.send("API running...");
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import {connectDB} from "./config/db.js"
 import router from "./routes/authRoutes.js";
-
- 
+import noteRouter from "./routes/noteRoutes.js"
  
 const port = process.env.PORT || 5000;
 //middleware
@@ -22,9 +21,11 @@ app.use(
 ); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/auth",router)
 app.use(cookieParser());
 
+
+app.use("/api/auth",router)
+app.use("/api")
 
 app.get("/", (req, res) => {
   res.send("API running...");
