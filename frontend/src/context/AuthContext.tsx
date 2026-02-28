6b57728162303f786aa682422694d65e20c23d02
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-28 17:40:04 +0530

    authProvider

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,12 +1,25 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import {type User, type AuthContextType } from "../types/AuthTypes"
 
 const AuthContext = createContext<AuthContextType|null>(null);
 
 const AuthProvider = ()=>{
     const [user,setUser] = useState<User|null>(null);
-    const [loading,setLoading] = useState(true)
+    const [loading,setLoading] = useState<boolean>(true);
+
+    useEffect(()=>{
+        const checkUser = async()=>{
+            try{
+
+            }catch(err){
+                console.log("Not logged in!")
+            }finally
+        }
+    },[])
+
     return(
-         
+        <>
+        
+        </>    
     )
 }
