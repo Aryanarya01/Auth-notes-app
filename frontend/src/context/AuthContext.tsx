f41628f2439106f7795c73c58fc916cb74d58e37
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-28 17:51:25 +0530

    authProvider

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,47 +1,53 @@
-import { createContext, use, useContext, useEffect, useState, type ReactNode } from "react"
-import {type User, type AuthContextType } from "../types/AuthTypes"
+import {
+  createContext,
+  use,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+import { type User, type AuthContextType } from "../types/AuthTypes";
 
-const AuthContext = createContext<AuthContextType|null>(null);
+const AuthContext = createContext<AuthContextType | null>(null);
 
-export const AuthProvider = ({children}:{children : React.ReactNode})=>{
-    const [user,setUser] = useState<User|null>(null);
-    const [loading,setLoading] = useState<boolean>(true);
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(()=>{
-        const checkUser = async()=>{
-            try{
-                const res = await fetch("http://localhost:5000/api/auth/profile",{
-                    credentials : "include",
-                });
-                if(res.ok){
-                    const data = await res.json();
-                    setUser(data);
-                }
-            }catch(err){
-                console.log("Not logged in!")
-            }finally{
-                setLoading(false);
-            }
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/auth/profile", {
+          credentials: "include",
+        });
+        if (res.ok) {
+          const data = await res.json();
+          setUser(data);
         }
-        checkUser();
-    },[])
-    const logout =async ()=>{
-        await fetch("http://localhost:5000/api/auth/logout",{
-            method : "POST",
-            credentials:"include"
-        })
-        setUser(null)
-    }
-    return(
-        <>
-            <AuthContext.Provider value={{user,setUser,loading,logout}}>
-                {children}
-            </AuthContext.Provider>
-        </>    
-    )
-}
+      } catch (err) {
+        console.log("Not logged in!");
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkUser();
+  }, []);
+  const logout = async () => {
+    await fetch("http://localhost:5000/api/auth/logout", {
+      method: "POST",
+      credentials: "include",
+    });
+    setUser(null);
+  };
+  return (
+    <>
+      <AuthContext.Provider value={{ user, setUser, loading, logout }}>
+        {children}
+      </AuthContext.Provider>
+    </>
+  );
+};
 
-export const useAuth = ()=>{
-    const context = useContext(AuthContext);
-
-}
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+};
