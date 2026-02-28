051703b993970434c5f5dd7daad05c4d28181380
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-28 17:35:39 +0530

    authProvider

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,11 @@
+import { createContext, useState } from "react"
+import { type AuthContextType } from "../types/AuthTypes"
+
+const AuthContext = createContext<AuthContextType|null>(null);
+
+const AuthProvider = ()=>{
+    const [user,setUser] = useState
+    return(
+         
+    )
+}
