b18bca4f1420f010a72f3cb97db413c1760ae130
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-28 10:21:15 +0530

    note route

--- backend/src/routes/noteRoutes.ts
+++ backend/src/routes/noteRoutes.ts
@@ -1,11 +1,9 @@
-
-
-import express, { Router } from "express"
+import express, { Router } from "express";
 import { createNote, getNotes } from "../controllers/notesController.js";
 import { protect } from "../middleware/protect.js";
 
-const router:Router = express.Router();
+const router: Router = express.Router();
 
-router.post("/",protect,createNote);
-router.get("/",protect,getNotes);   
+router.post("/", protect, createNote);
+router.get("/", protect, getNotes);
 export default router;
