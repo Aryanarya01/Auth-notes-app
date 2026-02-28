d4d7f90eb2c73fdc2052d5db8e312105b91f7cf8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-02-28 15:20:43 +0530

    deleting note

--- backend/dist/routes/noteRoutes.js
+++ backend/dist/routes/noteRoutes.js
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
-import { createNote, getNotes, updateNote } from "../controllers/notesController.js";
+import { createNote, deleteNote, getNotes, updateNote, } from "../controllers/notesController.js";
 import { protect } from "../middleware/protect.js";
 const router = express.Router();
 router.post("/", protect, createNote);
 router.get("/", protect, getNotes);
 router.put("/:id", protect, updateNote);
+router.delete("/:id", protect, deleteNote);
 export default router;
 //# sourceMappingURL=noteRoutes.js.map
--- backend/src/routes/noteRoutes.ts
+++ backend/src/routes/noteRoutes.ts
@@ -1,10 +1,16 @@
 import express, { Router } from "express";
-import { createNote, getNotes, updateNote } from "../controllers/notesController.js";
+import {
+  createNote,
+  deleteNote,
+  getNotes,
+  updateNote,
+} from "../controllers/notesController.js";
 import { protect } from "../middleware/protect.js";
 
 const router: Router = express.Router();
 
 router.post("/", protect, createNote);
 router.get("/", protect, getNotes);
-router.put("/:id",protect,updateNote)
+router.put("/:id", protect, updateNote);
+router.delete("/:id", protect, deleteNote);
 export default router;
