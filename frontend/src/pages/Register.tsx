d1c68d7b0dd151c53aa2cfc88e96194567aca254
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-01 13:05:00 +0530

    Register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,19 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const Register = ()=>{
+    const navigate = useNavigate();
+
+
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const handelRegister = async(e:React.FormEvent)=>{
+        const res = await
+    }
+     
     return(
         <>
         <h1>Register</h1>
