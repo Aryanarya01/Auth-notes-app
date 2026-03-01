6178bf164f554238998c12a80fd95fc7ae6cc4a9
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-01 13:14:20 +0530

    Register page completed

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,52 +1,69 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+const Register = () => {
+  const navigate = useNavigate();
 
-const Register = ()=>{
-    const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-
-    const handelRegister = async(e:React.FormEvent)=>{
-        e.preventDefault()
-        const res = await fetch("http://localhost:5000/api/auth/register",{
-            method : "POST",
-            headers : {
-                "Content-Type" : "application/json",
-            },
-            credentials : "include",
-            body : JSON.stringify({name,email,password})
-        });
-        const data = await res.json();
-        if(res.ok){
-            alert("Registered Successfully. Please login!");
-            navigate("/");
-        }else{
-            alert(data.message)
-        }
+  const handelRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:5000/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ name, email, password }),
+    });
+    const data = await res.json();
+    if (res.ok) {
+      alert("Registered Successfully. Please login!");
+      navigate("/");
+    } else {
+      alert(data.message);
     }
-     
-    return(
-        <>
-        <div>
-            <h1>Register</h1>
-            <form onSubmit={handelRegister}>
-                <input type="text" value={name} placeholder="Enter Name.." onChange={(e)=>setName(e.target.value)} />
-                <br /><br />
-                <input type="email" value={email} placeholder="Enter Email.." onChange={(e)=>setEmail(e.target.value)} />
-                <br /><br />
-                <input type="password" value={password} placeholder="Enter Password.." onChange={(e)=>setPassword(e.target.value)} />
-                <br /><br />
-                <button type="submit">Register</button>
-                <br />
-                <p>Already have account? <Link to="/" >Login</Link></p>
-            </form>
-        </div>
+  };
 
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={handelRegister}>
+          <input
+            type="text"
+            value={name}
+            placeholder="Enter Name.."
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <br />
+          <input
+            type="email"
+            value={email}
+            placeholder="Enter Email.."
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            value={password}
+            placeholder="Enter Password.."
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <br />
+          <button type="submit">Register</button>
+          <br />
+          <p>
+            Already have account? <Link to="/">Login</Link>
+          </p>
+        </form>
+      </div>
+    </>
+  );
+};
 export default Register;
