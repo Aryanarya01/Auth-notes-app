17bac97e00aa323b05ad9be2a99e19973973c2d2
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-02 14:27:45 +0530

    notes interface

--- frontend/src/pages/Notes.tsx
+++ frontend/src/pages/Notes.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import { Note } from "../types/NoteTypes";
+import {type Note } from "../types/NoteTypes";
 
 
 const Notes = ()=>{
