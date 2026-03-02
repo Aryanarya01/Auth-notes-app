ed57024589f292379c586b5855880e08c6ff7705
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-02 14:25:59 +0530

    notes interface

--- frontend/src/pages/Notes.tsx
+++ frontend/src/pages/Notes.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
+import { Note } from "../types/NoteTypes";
 
 
 const Notes = ()=>{
     const {logout} = useAuth();
+    const [notes,setNotes] = useState<Note[]>([]);
+    const [title,setTitle] = useState("");
+    const [content,setContent] = useState("");
     return(
         <>
         <h1>Notes</h1>
