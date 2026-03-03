6ca64b4e17f1700ce722277aa0ecba6ca674a5d7
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-03 13:50:04 +0530

    fixed mistake

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,11 +1,29 @@
 
 
-const Profile = ()=>{
-    return(
-        <>
-        <h1>Profile</h1>
-        </>
-    )
-}
+// const Profile = ()=>{
+//     return(
+//         <>
+//         <h1>Profile</h1>
+//         </>
+//     )
+// }
+
+// export default Profile;
+import { useAuth } from "../context/AuthContext";
+
+const Profile = () => {
+  const { user, logout } = useAuth();
+
+  return (
+    <div>
+      <h2>Profile</h2>
+
+      <p><strong>Name:</strong> {user?.name}</p>
+      <p><strong>Email:</strong> {user?.email}</p>
+
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+};
 
 export default Profile;
