b252cfe09264d16a22a3c4cad192df1ce87cf1a1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-05 16:46:25 +0530

    Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,19 +1,18 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-    const Navbar = ()=>{
-        const {logout, user}  = useAuth();
+const Navbar = () => {
+  const { logout, user } = useAuth();
 
-        return(
-            <nav>
-                <h3>Notes App</h3>
-                <div>
-                    <Link to="/notes">Notes</Link>|{" "}
-                    <Link  to="/profile" >Profile</Link>|{" "}
-                    {user && <button onClick={logout}>Logout</button>}
-                </div>
-            </nav>
-        )
-    }
+  return (
+    <nav>
+      <h3>Notes App</h3>
+      <div>
+        <Link to="/notes">Notes</Link>| <Link to="/profile">Profile</Link>|{" "}
+        {user && <button onClick={logout}>Logout</button>}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
