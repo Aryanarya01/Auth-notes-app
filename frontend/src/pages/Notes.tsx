b918367da652623d4d1baadf5c997a8296ef0810
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-05 16:25:36 +0530

    fixed mistake

--- frontend/src/pages/Notes.tsx
+++ frontend/src/pages/Notes.tsx
@@ -7,7 +7,7 @@ const Notes = () => {
   const [notes, setNotes] = useState<Note[]>([]);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [editingId,setEditingId] = useState<string|null>(null)
+  const [editingId, setEditingId] = useState<string | null>(null);
 
   //fetch notes
   const fetchNotes = async () => {
@@ -47,54 +47,55 @@ const Notes = () => {
       method: "DELETE",
       credentials: "include",
     });
-      console.log("Status:", res.status);
+    console.log("Status:", res.status);
 
-  const data = await res.json();
-  console.log("Response:", data);
-    if(res.ok){
-        setNotes((prev)=>prev.filter((note)=>note._id !== id))
+    const data = await res.json();
+    console.log("Response:", data);
+    if (res.ok) {
+      setNotes((prev) => prev.filter((note) => note._id !== id));
     }
   };
 
-  const handelEdit =async (e:React.FormEvent)=>{
+  const handelEdit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if(editingId){
-        //update
-        const res = await fetch(`http://localhost:5000/api/notes/${editingId}`,{
-          method : "PUT",
-          credentials : "include",
-          headers : {
-            "Content-Type" : "application/json",
-          },
-          body : JSON.stringify({title,content})
-        });
-        if(res.ok){
-          const updatedNote = await res.json();
+    if (editingId) {
+      //update
+      const res = await fetch(`http://localhost:5000/api/notes/${editingId}`, {
+        method: "PUT",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, content }),
+      });
+      if (res.ok) {
+        const updatedNote = await res.json();
 
-          setNotes((prev)=>prev.map((note)=>note._id === editingId ? updatedNote : note))
-          setEditingId(null);
-          setTitle("");
-          setContent("")
-        }
-    }else{
-       // CREATE
-    const res = await fetch("http://localhost:5000/api/notes", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      credentials: "include",
-      body: JSON.stringify({ title, content }),
-    });
+        setNotes((prev) =>
+          prev.map((note) => (note._id === editingId ? updatedNote : note)),
+        );
+        setEditingId(null);
+        setTitle("");
+        setContent("");
+      }
+    } else {
+      // CREATE
+      const res = await fetch("http://localhost:5000/api/notes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({ title, content }),
+      });
 
-    if (res.ok) {
-      const newNote = await res.json();
-      setNotes((prev) => [newNote, ...prev]);
-      setTitle("");
-      setContent("");
+      if (res.ok) {
+        const newNote = await res.json();
+        setNotes((prev) => [newNote, ...prev]);
+        setTitle("");
+        setContent("");
+      }
     }
-  }
-    
-  }
+  };
 
   return (
     <>
@@ -113,7 +114,9 @@ const Notes = () => {
           value={content}
           onChange={(e) => setContent(e.target.value)}
         />
-        <button type="submit">{editingId ? "Update Note.." : "Add Note.."}</button>
+        <button type="submit">
+          {editingId ? "Update Note.." : "Add Note.."}
+        </button>
       </form>
       <hr />
 
@@ -122,11 +125,15 @@ const Notes = () => {
           <h4>{note.title}</h4>
           <h4>{note.content}</h4>
           <button onClick={() => handelDelete(note._id)}>Delete</button>
-          <button onClick={()=>{
-            setEditingId(note._id),
-            setTitle(note.title),
-            setContent(note.content)
-          }}>Edit</button>
+          <button
+            onClick={() => {
+              (setEditingId(note._id),
+                setTitle(note.title),
+                setContent(note.content));
+            }}
+          >
+            Edit
+          </button>
           <hr />
         </div>
       ))}
