14c7a18fe470cf85d1311e2a672e32e2b30a112f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-05 16:45:31 +0530

    Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
     const Navbar = ()=>{
@@ -5,7 +6,11 @@ import { useAuth } from "../context/AuthContext";
 
         return(
             <nav>
-                
+                <h3>Notes App</h3>
+                <div>
+                    <Link to="/notes">Notes</Link>|{" "}
+                    <Link  to="/profile" >Profile</Link>|{" "}
+                </div>
             </nav>
         )
     }
